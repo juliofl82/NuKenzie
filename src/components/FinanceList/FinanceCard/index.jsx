fbc6c867ff8322ccd30446dae95e9df4c4f9e517
juliofl82 <jfigueiredolamounier@gmail.com>
2024-04-17 13:32:57 -0300

    correcao_botao-exclusao

--- src/components/FinanceList/FinanceCard/index.jsx
+++ src/components/FinanceList/FinanceCard/index.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import styles from './FinanceCard.module.scss'; 
+import styles from './FinanceCard.module.scss';
 
 export const FinanceCard = ({ transaction, onDelete }) => {
 
+    const formatCurrency = (value) => {
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value);
+    };
     const colorClass = transaction.tipo === "entrada" ? styles.entrada : styles.despesa;
 
     return (
         <div className={styles.financeCard}>
-            <div className={`${styles.colorCard} ${colorClass}`}>                
+            <div className={`${styles.colorCard} ${colorClass}`}>
             </div>
             <div className={styles.descricao}>
                 <p>Descrição: {transaction.descricao}</p>
                 <p className={styles.tipoEntrada} >{transaction.tipo}</p>
             </div>
             <div className={styles.valor}>
-                <p>R${transaction.valor.toFixed(2)}</p>
+                <p>{formatCurrency(transaction.valor)}</p>
                 <button onClick={onDelete}>Excluir</button>
             </div>
         </div>
